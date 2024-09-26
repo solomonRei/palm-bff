77bbacc75dd71b671e30058d84b0f040ccc1f722
Eduard <dz6class@mail.ru>
2024-09-26 14:03:55 +0300

    Added custom filter

---
+++ src/main/java/com/palm/bff/ResponseCustomFilterFactory.java
@@ -0,0 +1,62 @@
+package com.palm.bff;
+
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.rewrite.ModifyResponseBodyGatewayFilterFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class ResponseCustomFilterFactory extends AbstractGatewayFilterFactory<ResponseCustomFilterFactory.Config> {
+
+    @Getter
+    public static class Config {
+        private String param1;
+
+    }
+
+    private final ModifyResponseBodyGatewayFilterFactory modifyResponseBodyFilterFactory;
+    private final ObjectMapper objectMapper;
+
+    public ResponseCustomFilterFactory(ModifyResponseBodyGatewayFilterFactory modifyResponseBodyFilterFactory, ObjectMapper objectMapper) {
+        super(Config.class);
+        this.modifyResponseBodyFilterFactory = modifyResponseBodyFilterFactory;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        ModifyResponseBodyGatewayFilterFactory.Config modifyResponseBodyFilterFactoryConfig = new ModifyResponseBodyGatewayFilterFactory.Config();
+
+        modifyResponseBodyFilterFactoryConfig.setNewContentType(MediaType.APPLICATION_JSON_VALUE);
+        modifyResponseBodyFilterFactoryConfig.setRewriteFunction(Map.class, Map.class, (exchange, bodyAsMap) -> {
+            try {
+                Map<String, Object> responseBody = (Map<String, Object>) bodyAsMap;
+                log.info("Response body: {}", responseBody);
+
+
+                return Mono.just(responseBody);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Mono.error(e);
+            }
+        });
+
+        return modifyResponseBodyFilterFactory.apply(modifyResponseBodyFilterFactoryConfig);
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return List.of("param1");
+    }
+}
+
---
+++ src/main/java/com/palm/bff/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.palm.bff;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(auth -> auth
+                        .pathMatchers("/**").permitAll()
+                )
+//                .csrf(csrf -> csrf
+//                        .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+//                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable);
+
+        return http.build();
+    }
+}
+
